22e19f6627df8306c9189dc7ab73fea03acd82b7
ShreySham <shreysham@gmail.com>
2023-04-15 13:13:49 -0500

    Changed to Selenium and made default game settings 1 min, no move

--- src/geoguessr.js
+++ src/geoguessr.js
@@ -1,5 +1,3 @@
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
@@ -9,28 +7,16 @@ const fs = require('fs');
 async function loginSaveCookie(tab){
   try {
     await tab.goto('https://www.geoguessr.com/signin');
-    await tab.type('email', process.env.GEO_EMAIL);
-    await tab.type('password', process.env.GEO_EMAIL);
-    await tab.click('.form-field_formField__beWhf form-field_typeActions__tMY1O');
-    await tab.waitForSelector('[content="https://www.geoguessr.com/"]');
+    await tab.type('[name="email"]', process.env.GEO_EMAIL);
+    await tab.type('[name="password"]', process.env.GEO_PASS);
+    const navigationPromise = tab.waitForNavigation();
+    await tab.click('[data-qa="login-cta-button"]');
+    await navigationPromise;
     const cookie = await tab.cookies();
-    fs.writeFileSync('./cookes.json', JSON.stringify(cookie));
-
-  /*
-    await tab.get("https://www.geoguessr.com/signin");
-    await tab.findElement(webdriver.By.name("email")).sendKeys(process.env.GEO_EMAIL);
-    await tab.findElement(webdriver.By.name("password")).sendKeys(process.env.GEO_PASS);
-    await tab.findElement(webdriver.By.className("button_button__CnARx button_variantPrimary__xc8Hp")).click();
-    await tab.wait(webdriver.until.titleIs("GeoGuessr - Let's explore the world!"), 10000);
-    let allCookies = await tab.manage().getCookies();
-    for(const cookie of allCookies){
-      fs.writeFileSync('./cookies.txt', JSON.stringify(cookie) + '\n', { flag: 'a+' }, err => {});
-    }
-  */
+    fs.writeFileSync('./cookies.json', JSON.stringify(cookie), { flag: 'a+' }, err => {console.error(err)});
   } catch(error) {
     console.error("Error logging in: " + error);
   }
-
 }
 
 /**
@@ -41,14 +27,14 @@ async function getChallengeLink(optionalMap) {
 
   let mapName = optionalMap || "A Diverse World";
   let challengeLink = "";
-  //let browser = new webdriver.Builder().forBrowser('chrome');
-  //browser.setChromeOptions(new chrome.Options().addArguments('--headless=chrome', '--no-sandbox', '--single-process').windowSize({width: 1920, height: 1080}));
-  //let tab = browser.build();
+
   try{
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [`--window-size=1920,1080`]
+    });
     const tab = await browser.newPage();
     if (fs.existsSync('./cookies.json')) {
-      const cookies = JSON.parse(await fs.readFile('./cookies.json', 'utf8'));
+      const cookies = JSON.parse(fs.readFileSync('./cookies.json', 'utf8'));
       if (cookies[0].expiry < Math.round(Date.now() / 1000)) {
         fs.unlinkSync('./cookies.json');
         await loginSaveCookie(tab);
@@ -59,27 +45,67 @@ async function getChallengeLink(optionalMap) {
     await tab.deleteCookie();
     var fileBuffer = fs.readFileSync('./cookies.json');
     var allCookies = JSON.parse(fileBuffer);
-    tab.setCookie(...allCookies);
-    await tab.goto("https://www.geoguessr.com/");
-    await tab.waitForFunction('document.querySelector');
-    await tab.wait(webdriver.until.titleIs("GeoGuessr - Let's explore the world!"), 10000);
+    await tab.setCookie(...allCookies);
+    await Promise.all([
+      tab.waitForNavigation({timeout: 10000}),
+      tab.goto("https://www.geoguessr.com/"),
+    ])
     console.log("gotHome");
-    await tab.wait(await webdriver.until.elementLocated(await webdriver.By.className("quick-search_searchInputButton__kK9Hz")), 2000).click();
-    await tab.wait(await webdriver.until.elementLocated(await webdriver.By.className("quick-search_searchInput__i7C6O")), 2000).click();
-    await tab.findElement(webdriver.By.className("quick-search_searchInput__i7C6O")).sendKeys(mapName);
-    await tab.wait(await webdriver.until.elementLocated(await webdriver.By.className("search-result_wrapper__jwHxo search-result_isFocused__ZAXVX")), 2000).click();
-    await tab.wait(webdriver.until.titleContains("Map"), 10000);
-    console.log("gotMap");
-    await tab.findElement(webdriver.By.className("button_button__CnARx button_variantPrimary__xc8Hp button_sizeLarge__Va8YG")).click();
-    await tab.wait(webdriver.until.titleContains("Start a game"), 10000);
-    await tab.findElement(webdriver.By.css('*[data-qa="game-type-challenge"]')).click();
-    await tab.findElement(webdriver.By.css('*[data-qa="invite-friends-button"]')).click();
-    await tab.wait(await webdriver.until.elementLocated(await webdriver.By.className("text-input_textInput__HPC_k")), 2000).click();
-    challengeLink = await tab.findElement(webdriver.By.className("text-input_textInput__HPC_k")).getAttribute("value");
+    await Promise.all([
+      tab.waitForSelector('.quick-search_searchInput__i7C6O',{timeout: 3000}),
+      tab.click(".quick-search_searchInputButton__kK9Hz"),
+    ])
+    await tab.type(".quick-search_searchInput__i7C6O", mapName);
+    await tab.waitForSelector('.quick-search_resultList__cLjml',{timeout: 5000});
+    
+    await Promise.all([
+      tab.waitForNavigation({timeout: 3000}),
+      tab.click('li[class="search-result_wrapper__jwHxo search-result_isFocused__ZAXVX"]'),
+    ]);
+
+    await Promise.all([
+      tab.waitForNavigation({timeout: 3000}),
+      tab.goto(tab.url()+'/play'),
+    ]);
+
+    await tab.evaluate(() => {
+      document.querySelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > div.section_sectionMedium__yXgE6 > div > div > div:nth-child(2) > input").click();
+    });
+
+    await tab.evaluate(() => {
+      document.querySelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > div.section_sectionMedium__yXgE6 > div > div.game-options_optionGroup__qNKx1 > div > div.game-options_wrappingGroup__u3pGi > label:nth-child(1) > div.game-options_optionInput__TAqdI > input").click();
+    });
+
+    const sliderSelector = ".styles_handle__zYRZ7"
+    const sliderElement = await tab.$(sliderSelector);
+    var slider = await tab.waitForSelector(sliderSelector);
+    await tab.evaluate((item) => item.scrollIntoView(), slider);
+    const pos = await sliderElement.boundingBox();
+
+    await tab.mouse.move(pos.x + pos.width / 2, pos.y + pos.height / 2);
+    await tab.mouse.down();
+    await tab.mouse.move(pos.x + 2.0 , pos.y + pos.height / 2);
+    await tab.mouse.up();
+    await tab.mouse.down();
+    await tab.mouse.up();
+
+
+    await tab.evaluate(() => {
+      document.querySelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > div:nth-child(2) > div:nth-child(2) > label > input[type=radio]").click();
+    });
+
+    await Promise.all([
+      tab.waitForSelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > section > article > div > span > input",{timeout: 10000}),
+      tab.evaluate(() => {
+        document.querySelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > div.start-challenge-game_body__sWqnL > button > div").click();
+      }),
+    ]);
+
+    const linkBox = await tab.waitForSelector("#__next > div.background_wrapper__OlrxG.background_backgroundClassic__ySr_Z > div.version4_layout__KcIcs > div.version4_content__oaYfe > main > div > div > div > div > section > article > div > span > input");
+    challengeLink = await tab.evaluate(x => x.value, linkBox);
   } catch(error) {
-    console.error("Error navigating GeoGuessr: " + error);
-  } finally {
-    await tab.close();
+    console.error("Error navigating GeoGuessr: ");
+    throw error;
   }
   console.log(challengeLink);
   return challengeLink;
